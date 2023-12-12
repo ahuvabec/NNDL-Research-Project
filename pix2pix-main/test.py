2dbfbb683e21a3e14d4f9c9d2243ccb5c004c9cd
maitarasher <maitarasher@gmail.com>
2023-12-12 11:37:24 +0200

    fix move to cpu

--- pix2pix-main/test.py
+++ pix2pix-main/test.py
@@ -96,13 +96,13 @@ with torch.no_grad():
             # (3) The / 2 operation is a scaling factor that brings the pixel values into a standard image range of [0, 1].
             # input_image = inverse_transform(x.squeeze().cpu())
             # input_image = inverse_transform(x.detach().squeeze().cpu())
-            input_image = (x[0].data.numpy().transpose(1, 2, 0) + 1) / 2
+            input_image = (x[0].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2
             # generated_image = inverse_transform(output.squeeze().cpu())
             # generated_image = inverse_transform(output.detach().squeeze().cpu())
-            generated_image = (output[0].data.numpy().transpose(1, 2, 0) + 1) / 2
+            generated_image = (output[0].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2
             # real_image = inverse_transform(real.squeeze().cpu())
             #real_image = inverse_transform(real.detach().squeeze().cpu())
-            real_image = (real[0].data.numpy().transpose(1, 2, 0) + 1) / 2
+            real_image = (real[0].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2
 
 
             # Display the images
